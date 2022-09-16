2a66c41342d43beb91e846f7139b855573a272c4
Nate Lampros <97368611+natelampros@users.noreply.github.com>
2022-09-16 15:55:44 -0600

    Create main.cpp

---
+++ Q1.Reverse String/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string SwapLetters (string word) {
+    string swappedword = "";
+
+    for (int i = word.length() - 1; i >= 0; --i) {
+        swappedword.push_back(word.at(i));
+    }
+
+    return swappedword;
+}
+
+
+int main() {
+    string word;
+
+    cin >> word;
+
+    cout << SwapLetters(word);
+}
